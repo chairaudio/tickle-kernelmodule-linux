0cd2b1df8aa281213590972aeae3d3fbc70e27fd
Sebastian Stang <sebastian.stang@chair.audio>
2018-09-25 16:56:41 +0200

    Remove debug output when an led event is sent from the client

--- src/tickle_io.c
+++ src/tickle_io.c
@@ -167,14 +167,14 @@ long _tickle_io_ioctl(struct file* filp,
             TickleClient* client = filp->private_data;
             uint32_t* index_ptr;
             uint32_t* color_ptr;
-            printk(KERN_INFO "TICKLE_IOC_SET_COLOR\n");
+            // printk(KERN_INFO "TICKLE_IOC_SET_COLOR\n");
             if (copy_from_user(&mini_buffer, (void __user*)argp,
-                                 sizeof(MiniBuffer))) {
-                                   return -EFAULT;  
-                                 }
+                               sizeof(MiniBuffer))) {
+                return -EFAULT;
+            }
             index_ptr = (uint32_t*)&mini_buffer.data[0];
             color_ptr = (uint32_t*)&mini_buffer.data[4];
-            printk(KERN_INFO "OK : %d %d\n", *index_ptr, *color_ptr);
+            // printk(KERN_INFO "OK : %d %d\n", *index_ptr, *color_ptr);
             tickle_usb_send(NULL, client->device->context, &mini_buffer);
             return 0;
         } break;
